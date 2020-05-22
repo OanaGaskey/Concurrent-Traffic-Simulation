73ae71413c20350e9705fbb4f592ee066de4edbd
Oana <oana.gaskey@gmail.com>
2020-05-22 16:27:45 -0400

    defined message send mechanism

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -12,14 +12,17 @@ T MessageQueue<T>::receive()
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
 }
-
+*/
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+  	std::lock_guard<std::mutex> lock(_mutex);
+  	_queue.emplace_back(std::move(msg));
+  	_condition.notify_one();
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
