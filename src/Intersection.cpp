abf298113d753500fe5f43616ffd3ed6f176f0c2
Oana <oana.gaskey@gmail.com>
2020-05-26 20:53:35 -0400

    optimized lock release after printing

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -85,11 +85,9 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     ftrVehicleAllowedToEnter.wait();
     lck.lock();
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
-    
+    lck.unlock();
     // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
     if (_trafficLight.getCurrentPhase() == TrafficLightPhase::red) _trafficLight.waitForGreen();
-
-    lck.unlock();
 }
 
 void Intersection::vehicleHasLeft(std::shared_ptr<Vehicle> vehicle)
